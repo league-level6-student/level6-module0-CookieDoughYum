4e1a0496e2a3483f1258d1e8e3829a0cfd7e64f7
root <root@306155dcd585>
2021-11-27 02:27:13 +0000

    Working on DeliveryDriverTest - itShouldRefuel method

--- src/test/java/_06_payroll/PayrollTest.java
+++ src/test/java/_06_payroll/PayrollTest.java
@@ -1,38 +1,54 @@
 package _06_payroll;
 
-import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PayrollTest {
+import org.junit.jupiter.api.Test;
+
+public class PayrollTest {
 
     Payroll payroll = new Payroll();
 
     @Test
-    void itShouldCalculatePaycheck() {
+    public void itShouldCalculatePaycheck() {
         //given
-
+        double hourlyWage=2;
+        int numHours=10;
+        int expected=20;
+        
         //when
-
+        double actual=payroll.calculatePaycheck(hourlyWage, numHours);
+        
         //then
+        assertEquals(expected, actual);
     }
 
     @Test
-    void itShouldCalculateMileageReimbursement() {
+    public void itShouldCalculateMileageReimbursement() {
         //given
-
+        int milesTraveled=10;
+        double expected=5.75;
+        
         //when
-
+        double actual=payroll.calculateMileageReimbursement(milesTraveled);
+        
         //then
+        assertEquals(expected, actual);
     }
 
     @Test
-    void itShouldCreateOfferLetter() {
+    public void itShouldCreateOfferLetter() {
         //given
+    	String employeeName="Bob";
+    	double hourlyWage=10;
+    	String expected="Hello " + employeeName + ", We are pleased to offer you an hourly wage of " + hourlyWage;
 
         //when
-
+        String actual=payroll.createOfferLetter(employeeName, hourlyWage);
+        
         //then
+        assertEquals(expected, actual);
     }
 
 }
--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -13,25 +13,43 @@ class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
 
+    @Mock
+    CellPhone cellPhone;
+    
+    @Mock 
+    Car car;
+    
     @BeforeEach
     void setUp() {
-
+    MockitoAnnotations.openMocks(this);
+    deliveryDriver=new DeliveryDriver("Bob", car, cellPhone, true, true, false);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+CellPhone cellPhone = new CellPhone();
+boolean ExpectedBrowsing=true;
+
+when(cellPhone.browseCatMemes()).thenReturn(true);
 
         //when
+boolean ActualBrowsing=cellPhone.browseCatMemes();
 
         //then
+assertEquals(ActualBrowsing, ExpectedBrowsing);
     }
 
     @Test
     void itShouldRefuel() {
         //given
+    	int octaneGrade = 10;
+    	boolean expectedRefuel=true;
+    	
+    	when(car.fillTank(octaneGrade)).thenReturn(true);
 
         //when
+    	double actualRefuel=car.getFuelLevel();
 
         //then
     }
